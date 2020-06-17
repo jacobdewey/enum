8bc6a71b884b5818ca3ed4e841fec9868bbca861
jacobdewey <66138103+jacobdewey@users.noreply.github.com>
2020-06-17 11:17:58 -0400

    Update pxt.json, main.blocks, main.ts, Enum.ts, Test.ts

---
+++ Enum.ts
@@ -0,0 +1,9 @@
+// Add your code here
+enum Enum{
+//% block="a"
+//% block="b"
+//% block="c"
+//% block="d"
+//% block="e"
+//% block="f"
+}
---
+++ Test.ts
@@ -0,0 +1,14 @@
+// Add your code here
+namespace hi{
+//% block
+export function g(a: Enum, c: number): void{
+basic.showNumber(c)
+
+
+
+
+
+}
+
+
+}
